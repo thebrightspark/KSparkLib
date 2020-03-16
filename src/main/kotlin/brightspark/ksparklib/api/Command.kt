169bd7e51ef800222b833c1bef4c9966eee4a7a4
Mark Passey <mpassey@amelco.co.uk>
2020-03-16 16:17:50 +0000

    Added javadocs to Command class

--- src/main/kotlin/brightspark/ksparklib/api/Command.kt
+++ src/main/kotlin/brightspark/ksparklib/api/Command.kt
@@ -1,8 +1,15 @@
 package brightspark.ksparklib.api
 
+import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import net.minecraft.command.CommandSource
 
+/**
+ * Interface for a command that can be registered using the custom overload of [CommandDispatcher.register]
+ */
 interface Command {
+	/**
+	 * The command builder which will actually be registered to vanilla's [CommandDispatcher.register]
+	 */
 	val builder: LiteralArgumentBuilder<CommandSource>
 }
