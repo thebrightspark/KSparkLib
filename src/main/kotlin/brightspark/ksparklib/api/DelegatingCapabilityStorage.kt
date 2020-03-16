5c5364d975ab17b6966b3b971910883dc8f05a1e
Mark Passey <mpassey@amelco.co.uk>
2020-03-16 17:46:51 +0000

    Added javadoc to DelegatingCapabilityStorage class

--- src/main/kotlin/brightspark/ksparklib/api/DelegatingCapabilityStorage.kt
+++ src/main/kotlin/brightspark/ksparklib/api/DelegatingCapabilityStorage.kt
@@ -6,6 +6,9 @@ import net.minecraft.util.Direction
 import net.minecraftforge.common.capabilities.Capability
 import net.minecraftforge.common.util.INBTSerializable
 
+/**
+ * A default [Capability.IStorage] implementation that delegates calls to the capability
+ */
 class DelegatingCapabilityStorage<T : INBTSerializable<CompoundNBT>> : Capability.IStorage<T> {
 	override fun writeNBT(capability: Capability<T>, instance: T, side: Direction?): INBT = instance.serializeNBT()
 
